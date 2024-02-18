e4280f2c02b18ae8ad6b80837f13a5044255820b
lunaesomnia <lunaesomnia@protonmail.com>
2024-02-18 21:06:45 +0100

    includes

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ rm -rf yay
 yay
 
 # Installing widgets and utils
-yay -S gnome gnome-extra hyprshot swayosd-git swaync anyrun-git wireplumber pavucontrol aylurs-gtk-shell ttf-jetbrains-mono htop
+yay -S xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland gnome gnome-extra hyprshot swayosd-git swaync anyrun-git wireplumber pavucontrol aylurs-gtk-shell ttf-jetbrains-mono htop
 
 sudo systemctl enable --now swayosd-libinput-backend
 
