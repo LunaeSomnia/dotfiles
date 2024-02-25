6353c6bbd8cefbb309af579988efe76617eb7ad1
lunaesomnia <lunaesomnia@protonmail.com>
2024-02-25 22:14:01 +0100

    25-02-24

--- init.sh
+++ init.sh
@@ -12,10 +12,12 @@ rm -rf yay
 
 yay
 
+echo '!!!!!! Dont forget to enable multilib for steam !!!!!!'
+
 # Installing widgets and utils
-yay -S xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland gnome gnome-extra hyprshot swayosd-git swaync anyrun-git wireplumber pavucontrol aylurs-gtk-shell ttf-jetbrains-mono htop
+yay -S xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland gnome gnome-extra gnome-control-center adw-gtk3 hyprshot swayosd-git swaync anyrun-git wireplumber pavucontrol aylurs-gtk-shell ttf-jetbrains-mono htop gvfs sassc rustup
 
 sudo systemctl enable --now swayosd-libinput-backend
 
 # Installing apps
-yay -S firefox code bitwarden
+yay -S vim qbittorrent vlc swww ffmpeg firefox code bitwarden spotify spotify-adblock lutris steam wine discord zsh obs-studio v4l2loopback-dkms linux-headers musescore muse-hub-bin prismlauncher
